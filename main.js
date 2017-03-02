328214434763ff123c8a150823b6144d7b322584
Gabriel Reis <gabriel.oreis@gmail.com>
2017-03-01 19:58:49 -0500

    Set status bar style globally

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ import Exponent from 'exponent';
 import { StackNavigator } from 'react-navigation';
 import Home from './src/components/Home';
 import {
+  StatusBar,
 } from 'react-native';
 
+StatusBar.setBarStyle('dark-content');
 
 const App = StackNavigator({
   Home: { screen: Home },
