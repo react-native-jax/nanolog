6ae7056a62dae6d256b991f50dd48adf3c89ddfb
Gabriel Reis <gabriel.oreis@gmail.com>
2017-03-01 19:58:06 -0500

    Use StackNavigator

--- main.js
+++ main.js
@@ -1,28 +1,12 @@
 import Exponent from 'exponent';
-import React from 'react';
+import { StackNavigator } from 'react-navigation';
+import Home from './src/components/Home';
 import {
-  StyleSheet,
-  Text,
-  View,
 } from 'react-native';
 
-class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const App = StackNavigator({
+  Home: { screen: Home },
 });
 
 Exponent.registerRootComponent(App);
---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+  View,
+  StyleSheet,
+} from 'react-native';
+
+class Home extends Component {
+  render() {
+    return <View>
+    </View>
+  }
+}
+
+Home.propTypes = {
+};
+
+const styles = StyleSheet.create({
+});
+
+export default Home;
