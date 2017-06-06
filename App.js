c391b57fc37ea265ef26970dd7d568b606542496
Gabriel Reis <gabriel.oreis@gmail.com>
2017-06-06 19:31:18 -0400

    Integrate with react-apollo

--- App.js
+++ App.js
@@ -2,12 +2,23 @@ import { StackNavigator } from 'react-navigation';
 import HomeScreen from './src/components/HomeScreen';
 import ItemScreen from './src/components/ItemScreen';
 import React from 'react';
-import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'remote-redux-devtools';
 import { persistStore, autoRehydrate } from 'redux-persist';
 import { AsyncStorage } from 'react-native';
+import {
+  ApolloClient,
+  ApolloProvider,
+  createNetworkInterface,
+} from 'react-apollo';
+
+const networkInterface = createNetworkInterface({
+  uri: 'http//localhost:3000/graphql',
+});
+const client = new ApolloClient({
+  networkInterface,
+});
 
 import reducers from './src/reducers';
 
@@ -16,9 +27,11 @@ const actualCompose = __DEV__ ? composeWithDevTools : compose;
 const store = createStore(
   combineReducers({
     ...reducers,
+    apollo: client.reducer(),
   }),
   {},
   actualCompose(
+    applyMiddleware(client.middleware()),
     applyMiddleware(thunk),
     autoRehydrate()
   )
@@ -33,9 +46,9 @@ const App = StackNavigator({
 
 const AppWithStore = () => {
   return (
-    <Provider store={store}>
+    <ApolloProvider store={store} client={client}>
       <App />
-    </Provider>
+    </ApolloProvider>
   );
 };
 
